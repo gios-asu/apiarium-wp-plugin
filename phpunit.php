0b64f682d22887bfdcb2b88ee5b39cb0462667a7
Ivan Montiel <idmontie@asu.edu>
2015-09-01 16:37:38 -0700

    Fix paths to tests

--- phpunit.php
+++ phpunit.php
@@ -3,10 +3,10 @@
 $_tests_dir = getenv( 'WP_TESTS_DIR' );
 
 if ( ! $_tests_dir ) {
-  $_tests_dir = '/tmp/wordpress';
+  $_tests_dir = '/tmp/wordpress/';
 }
 
-require_once $_tests_dir . '/includes/functions.php';
+require_once $_tests_dir . 'functions.php';
 
 function _manually_load_plugin() {
   require dirname( __FILE__ ) . '/apiarium.php';
@@ -14,4 +14,4 @@ function _manually_load_plugin() {
 
 tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
 
-require $_tests_dir . '/includes/bootstrap.php';
+require $_tests_dir . 'bootstrap.php';
