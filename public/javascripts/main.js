12aeb066b6793f4ab2baac52d666a7010549b956
Ryan Raub <ryan.raub@asu.edu>
2016-10-04 16:28:21 -0700

    whitespace and travis config update

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -2,45 +2,48 @@
  * Set the Bootstrap Carousel items to be the
  * height of their container
  */
-+function requireJquery( $ ) {
-  $( function onLoad() {
++function requireJquery($) {
+  $(function onLoad() {
     var timer = 20000; /* 20 seconds */
 
     +function setUpReset() {
-      setTimeout( function resetEveryTwoHours() {
+      setTimeout(function resetEveryTwoHours() {
         location.reload();
       }, 2 * 60 * 60 * 1000 /* 2 hour */ );
     }();
 
     +function setUpNoInteraction() {
-      $( 'html' ).css({
+      $('html').css({
         'overflow-y': 'hidden',
         'overflow-x': 'hidden'
       });
     }();
 
     +function setUpItemHeights() {
-      $( '.item' ).each( function setItemHeight( i, e ) {
-        var el, flexEl, closestSliderEl, height;
+      $('.item').each(function setItemHeight(i, e) {
+        var el,
+          flexEl,
+          closestSliderEl,
+          height;
 
-        el              = $( e );
-        flexEl          = el.closest( '.apiarium__flex' );
-        closestSliderEl = el.closest( '.apiarium__slider' );
+        el = $(e);
+        flexEl = el.closest('.apiarium__flex');
+        closestSliderEl = el.closest('.apiarium__slider');
 
         height = flexEl.height() - 10;
         // Substract the height of all other children
-        flexEl.children().not( closestSliderEl ).each( function ( i, e ) {
-          height -= $( e ).outerHeight();
-        } );
+        flexEl.children().not(closestSliderEl).each(function(i, e) {
+          height -= $(e).outerHeight();
+        });
 
-        el.css( 'height', height + 'px' );
+        el.css('height', height + 'px');
 
         var wordArray = e.innerHTML.split(' ');
-        while(e.scrollHeight > e.offsetHeight && wordArray.length > 0) {
+        while (e.scrollHeight > e.offsetHeight && wordArray.length > 0) {
           wordArray.pop();
           e.innerHTML = wordArray.join(' ') + '...';
         }
-      } );
+      });
     }();
 
     +function setUpCarousels() {
@@ -50,29 +53,29 @@
         keyboard: false
       });
     }();
-    
-    +function setUpTweets() {
-      setInterval( function animateTweets() {
-        $( '.apiarium__tweets' ).each( function ( _, tweetContainer ) {
-          $( tweetContainer ).find( '.apiarium__tweet' ).first()
-            .each( function animate ( i, e ) {
-            $(e).animate(
-              {
-                'margin-top': - $(e).height() + 'px'
-              },
-              'slow',
-              function after() {
-                $( e ).parent().append( $( e ) );
-                $( e ).css( 'margin-top', '0px' );
-              }
-            );
-          } );
-        } );
-      }, timer );
 
-      setInterval( function updateTimes() {
-        var $formatedTime = $( '.apiarium__tweet__date time' );
-        var calculateRelativeTime = function ( tweetTime ) {
+    +function setUpTweets() {
+      setInterval(function animateTweets() {
+        $('.apiarium__tweets').each(function(_, tweetContainer) {
+          $(tweetContainer).find('.apiarium__tweet').first()
+            .each(function animate(i, e) {
+              $(e).animate(
+                {
+                  'margin-top': -$(e).height() + 'px'
+                },
+                'slow',
+                function after() {
+                  $(e).parent().append($(e));
+                  $(e).css('margin-top', '0px');
+                }
+              );
+            });
+        });
+      }, timer);
+
+      setInterval(function updateTimes() {
+        var $formatedTime = $('.apiarium__tweet__date time');
+        var calculateRelativeTime = function(tweetTime) {
           var time = Math.round(+new Date() / 1000);
           var elapsedTime = time - tweetTime;
           var temp = '0h';
@@ -108,67 +111,67 @@
           return temp;
         };
 
-        $formatedTime.each(function ( i, e ) {
-          var oldTime = $( e ).html();
+        $formatedTime.each(function(i, e) {
+          var oldTime = $(e).html();
           // Ignore the case there the date is already formatted
           // as a month
           if (oldTime.length <= 3) {
-            var actualTime = $( e ).data('actualTime');
-            $( e ).html(calculateRelativeTime( actualTime ));
+            var actualTime = $(e).data('actualTime');
+            $(e).html(calculateRelativeTime(actualTime));
           }
         });
-      }, timer );
+      }, timer);
 
-      setInterval( function updateTweets() {
-        $('.apiarium__tweets').each( function updateEachTweetSection( i, e ) {
-          var query     = $( e ).attr( 'data-query' );
-          var queryType = $( e ).attr( 'data-query-type' );
-          var limit     = $( e ).attr( 'data-limit' );
+      setInterval(function updateTweets() {
+        $('.apiarium__tweets').each(function updateEachTweetSection(i, e) {
+          var query = $(e).attr('data-query');
+          var queryType = $(e).attr('data-query-type');
+          var limit = $(e).attr('data-limit');
 
-          var post_data = { 
-            action     : 'apiarium-twitter',
-            nonce      : apiarium.nonce,
-            serialized : JSON.stringify({
-              query : query,
+          var post_data = {
+            action: 'apiarium-twitter',
+            nonce: apiarium.nonce,
+            serialized: JSON.stringify({
+              query: query,
               queryType: queryType,
-              limit : limit
+              limit: limit
             }),
           };
 
           $.post(
-              apiarium.ajax_url,
-              post_data,
-              function success( response ) {
-                if ( response.success ) {
-                  $( e ).replaceWith( response.data.script_response );
-                }
-              },
-              'json'
+            apiarium.ajax_url,
+            post_data,
+            function success(response) {
+              if (response.success) {
+                $(e).replaceWith(response.data.script_response);
+              }
+            },
+            'json'
           );
         });
       }, 5 * 60 * 1000 /* 5 minutes */ );
     }();
 
     +function setUpClock() {
-      setInterval( function updateClock() {
-        var date         = new Date();
-        var hours        = date.getHours();
-        var minutes      = date.getMinutes();
-        var dateString   = date.toString();
-        var month        = dateString.substring(4, 7);
-        var currentDate  = dateString.substring(8, 10);
-        var ampm         = hours >= 12 ? 'PM' : 'AM';
-        hours            = hours % 12;
-        hours            = hours ? hours : 12; // the hour '0' should be '12'
-        hours            = hours;
-        minutes          = minutes < 10 ? '0' + minutes : minutes;
-        var strTime      = hours + ':' + minutes + ' ' + ampm;
+      setInterval(function updateClock() {
+        var date = new Date();
+        var hours = date.getHours();
+        var minutes = date.getMinutes();
+        var dateString = date.toString();
+        var month = dateString.substring(4, 7);
+        var currentDate = dateString.substring(8, 10);
+        var ampm = hours >= 12 ? 'PM' : 'AM';
+        hours = hours % 12;
+        hours = hours ? hours : 12; // the hour '0' should be '12'
+        hours = hours;
+        minutes = minutes < 10 ? '0' + minutes : minutes;
+        var strTime = hours + ':' + minutes + ' ' + ampm;
 
-        $( '.apiarium__calendar__time' ).html(strTime);
-        $( '.apiarium__calendar__date__icon__month' ).html(month);
-        $( '.apiarium__calendar__date__icon__date' ).html(currentDate);
-      }, timer );
+        $('.apiarium__calendar__time').html(strTime);
+        $('.apiarium__calendar__date__icon__month').html(month);
+        $('.apiarium__calendar__date__icon__date').html(currentDate);
+      }, timer);
     }();
 
-  } );
-}( jQuery );
+  });
+}(jQuery);
